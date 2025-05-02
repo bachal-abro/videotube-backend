bdeddac94dae21fe0859c46f9ed5848f67ffcb88
Muhammad Bachal <mbachalabro1@gmail.com>
2025-05-03 00:18:13 +0500

    Enhance comment functionality by adding user lookup in comments retrieval, improving error handling, and ensuring JWT verification for all comment routes

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,98 +1,161 @@
-import mongoose from "mongoose"
-import { Comment } from "../models/comment.model.js"
-import { ApiError } from "../utils/ApiError.js"
-import { ApiResponse } from "../utils/ApiResponse.js"
-import { asyncHandler } from "../utils/asyncHandler.js"
+import mongoose from "mongoose";
+import { Comment } from "../models/comment.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    const { videoId } = req.params
-    const { page = 1, limit = 10 } = req.query
+    const { videoId } = req.params;
+    const { page = 1, limit = 10 } = req.query;
 
     if (!videoId) {
-        throw new ApiError(400, "Invalid video id")
+        throw new ApiError(400, "Invalid video id");
     }
 
     const comments = await Comment.aggregate([
         { $match: { video: new mongoose.Types.ObjectId(videoId) } },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner",
+                pipeline:[
+                    {
+                        $project: {
+                            coverImage: 0,
+                            email: 0,
+                            watchHistory: 0,
+                            password: 0,
+                            createdAt: 0,
+                            updatedAt: 0,
+                            refreshToken: 0,
+                            __v: 0, // optionally remove version key too
+                        },
+                    },
+                ]
+            },
+        },
+        {
+            $lookup: {
+                from: "likes",
+                localField: "_id",
+                foreignField: "comment",
+                as: "likes",
+            }
+        },
+        {
+            $addFields: {
+                isLiked: {
+                    $cond: {
+                        if: { $in: [req.user?._id, "$likes.likedBy"] },
+                        then: true,
+                        else: false
+                    }
+                },
+                likes: {
+                    $size: "$likes"
+                }
+            }
+        },
+        {
+            $unwind: "$owner"
+        },
+        
         { $skip: (page - 1) * limit },
         { $limit: parseInt(limit) },
-    ])
+    ]);
 
     if (!comments) {
-        throw new ApiError(400, "comments not found")
+        throw new ApiError(400, "comments not found");
     }
 
     return res
         .status(200)
-        .json(new ApiResponse(200, comments, { totalCommentsCount: comments.length, page: page, limit: limit }, "comments fetched successfully"))
-})
+        .json(
+            new ApiResponse(
+                200,
+                comments,
+                {
+                    totalCommentsCount: comments.length,
+                    page: page,
+                    limit: limit,
+                },
+                "comments fetched successfully"
+            )
+        );
+});
 
 const addComment = asyncHandler(async (req, res) => {
-    const userId = req?.user?._id
-    const { videoId } = req.params
-    const { content } = req.body
+    const userId = req?.user?._id;
+    const { videoId } = req.params;
+    const { content } = req.body;
 
     if (!userId) {
-        throw new ApiError(400, "Invalid user ID")
+        throw new ApiError(400, "Invalid user ID");
     }
     if (!videoId) {
-        throw new ApiError(400, "Invalid video ID")
+        throw new ApiError(400, "Invalid video ID");
     }
     if (!content) {
-        throw new ApiError(400, "Comments cantent is required")
+        throw new ApiError(400, "Comments content is required");
     }
-    const comment = await Comment.create({ owner: userId, video: videoId, content: content })
+    const comment = await Comment.create({
+        owner: userId,
+        video: videoId,
+        content: content,
+    });
 
     if (!comment) {
-        throw new ApiError(400, " Error creating comment")
+        throw new ApiError(400, " Error creating comment");
     }
 
     return res
         .status(200)
-        .json(new ApiResponse(200, comment, {}, "Comment created successfully"))
-})
+        .json(
+            new ApiResponse(200, comment, {}, "Comment created successfully")
+        );
+});
 
 const updateComment = asyncHandler(async (req, res) => {
-    const { commentId } = req.params
-    const { content } = req.body
+    const { commentId } = req.params;
+    const { content } = req.body;
 
     if (!commentId) {
-        throw new ApiError(400, "Invalid comment ID")
+        throw new ApiError(400, "Invalid comment ID");
     }
 
-    const comment = await Comment.findByIdAndUpdate(commentId, { $set: { content: content } }, { new: true })
+    const comment = await Comment.findByIdAndUpdate(
+        commentId,
+        { $set: { content: content } },
+        { new: true }
+    );
 
     if (!comment) {
-        throw new ApiError(400, " Error updating comment")
+        throw new ApiError(400, " Error updating comment");
     }
 
-    res
-        .status(200)
-        .json(new ApiResponse(200, comment, {}, "Comment updated successfully"))
-})
+    res.status(200).json(
+        new ApiResponse(200, comment, {}, "Comment updated successfully")
+    );
+});
 
 const deleteComment = asyncHandler(async (req, res) => {
-    const { commentId } = req.params
+    const { commentId } = req.params;
 
     if (!commentId) {
-        throw new ApiError(400, "Invalid comment ID")
+        throw new ApiError(400, "Invalid comment ID");
     }
 
-    const comment = await Comment.findByIdAndDelete(commentId)
+    const comment = await Comment.findByIdAndDelete(commentId);
 
     if (!comment) {
-        throw new ApiError(400, " Error deleting comment")
+        throw new ApiError(400, " Error deleting comment");
     }
 
-    res
-        .status(200)
-        .json(new ApiResponse(200, comment, {}, "Comment deleted successfully"))
-})
+    res.status(200).json(
+        new ApiResponse(200, comment, {}, "Comment deleted successfully")
+    );
+});
 
-
-export {
-    getVideoComments,
-    addComment,
-    updateComment,
-    deleteComment
-}
+export { getVideoComments, addComment, updateComment, deleteComment };
